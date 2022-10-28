45f9c6f84fcf8a9175575821017fb0cfd1c4a99b
Keyu Duan <104810828@qq.com>
2022-10-28 20:30:24 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # G-BERT
+
 G-BERT: Textual Graph Pretraining with Large Language Models
+
+## Environment
+
+```bash
+conda install pytorch==1.12.0 torchvision==0.13.0 torchaudio==0.12.0 cudatoolkit=11.6 -c pytorch -c conda-forge
+conda install pyg -c pyg
+pip install transformers
+```
--- src/options.py
+++ src/options.py
@@ -6,20 +6,25 @@ def parse_args():
     parser = argparse.ArgumentParser()
     # environment
     parser.add_argument("--mode", type=str, default="train", choices=["train", "test"])
-    parser.add_argument("--world_size", type=int, default=1)
+    parser.add_argument("--world_size", type=int, default=4)
     parser.add_argument("--random_seed", type=int, default=123)
     parser.add_argument("--cont", type=bool, default=False)
 
     # parameters for data and model storage
-    parser.add_argument("--data_folder", type=str, default="./data/")
-    parser.add_argument("--model_type", type=str, default="G-BERT")
-    parser.add_argument("--model_dir", type=str, default="./ckpt")  # path to save
+    parser.add_argument("--data_folder", type=str, default="/raid/common")
+    parser.add_argument("--dataset", type=str, default="ogbn-arxiv")
+    parser.add_argument("--model_type", type=str, default="TGRoberta")
+    parser.add_argument("--ckpt_dir", type=str, default="./ckpt")  # path to save
+    # parser.add_argument("--config_name", type=str, default="#TODO")
     parser.add_argument("--pretrained_dir", type=str, default="./pretrained")
-    parser.add_argument("--ckpt_name", type=str, default="./ckpt/G-BERT/epoch-1.pt")
+    parser.add_argument("--pretrained_model", type=str, default="roberta")
 
     # essential hyperparameters
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--weight_decay", type=float, default=0.0)
+    parser.add_argument("--batch_size", type=int, default=20)
+    parser.add_argument("--num_parts", type=int, default=1500)
+    parser.add_argument("--epochs", type=int, default=100)
 
     args = parser.parse_args()
     logging.info(args)
