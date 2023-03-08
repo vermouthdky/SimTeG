a165c532ae0a0a490be0f90e5a0d6c31bb1a299b
Keyu Duan <104810828@qq.com>
2023-03-08 15:11:36 +0800

    reformat shells

--- optuna/gamlp_x_ogb.sh
+++ optuna/gamlp_x_ogb.sh
@@ -2,13 +2,8 @@ model_type='GAMLP'
 dataset='ogbn-arxiv'
 suffix='hp_x_ogb'
 
-epochs=500
-batch_size=10000
-eval_batch_size=10000
-eval_interval=5
-
-bash optuna/run.sh $model_type $dataset $suffix \
-    $epochs \
-    $batch_size \
-    $eval_batch_size \
-    $eval_interval
+bash optuna/run.sh --model_type $model_type --dataset $dataset --suffix $suffix \
+    --epochs 500 \
+    --batch_size 10000 \
+    --eval_batch_size 10000 \
+    --eval_interval 5
--- scripts/ogbn-arxiv/gamlp.sh
+++ scripts/ogbn-arxiv/gamlp.sh
@@ -6,6 +6,8 @@ suffix='bert_x_JK_GAMLP'
 # searched by optuna, valid acc: 70.30 %
 # with the same HPs, test on bert_x, valid_acc ~76%
 
+#! --use_bert_x
+
 bash scripts/train.sh --model_type $model_type --dataset $dataset --suffix $suffix \
     --eval_interval 5 \
     --lr 0.01 \
