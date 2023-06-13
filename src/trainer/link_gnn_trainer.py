508bf8ddc10e7d34b7fdd32b5872192db4442b7d
Keyu Duan <104810828@qq.com>
2023-06-13 14:43:06 +0800

    tem save

--- src/trainer/link_gnn_trainer.py
+++ src/trainer/link_gnn_trainer.py
@@ -1,13 +1,22 @@
 import argparse
 import logging
+import math
+import os.path as osp
+from itertools import chain
 
+import numpy as np
 import optuna
 import torch
 import torch.nn.functional as F
 from ogb.linkproppred import Evaluator, PygLinkPropPredDataset
+from scipy.sparse.csgraph import shortest_path
+from sklearn.metrics import roc_auc_score
 from torch.utils.data import DataLoader
-from torch_geometric.data import NeighborSampler
-from torch_geometric.transforms import ToUndirected
+from torch_geometric.data import Data, InMemoryDataset, NeighborSampler
+from torch_geometric.datasets import Planetoid
+from torch_geometric.loader import DataLoader
+from torch_geometric.transforms import RandomLinkSplit, ToSparseTensor, ToUndirected
+from torch_geometric.utils import k_hop_subgraph, to_scipy_sparse_matrix
 from tqdm.auto import tqdm
 
 from ..model import get_model_class
@@ -317,3 +326,185 @@ class LinkMLPTrainer(LinkTrainer):  # single GPU
             total_examples += num_examples
 
         return total_loss / total_examples
+
+
+#####################################
+# GCNTrainer (Full batch)
+#####################################
+
+
+class LinkGCNTrainer(LinkTrainer):
+    def __init__(self, *args, **kwargs):
+        super(LinkGCNTrainer, self).__init__(*args, **kwargs)
+        self.data = ToSparseTensor()(self.data)
+        # precompute GNN normalization
+        adj_t = self.data.adj_t.set_diag()
+        deg = adj_t.sum(dim=1).to(torch.float)
+        deg_inv_sqrt = deg.pow(-0.5)
+        deg_inv_sqrt[deg_inv_sqrt == float("inf")] = 0
+        adj_t = deg_inv_sqrt.view(-1, 1) * adj_t * deg_inv_sqrt.view(1, -1)
+        self.data.adj_t = adj_t
+        # set device
+        self.data.x = self.data.x.to(self.device)
+        self.data.adj_t = self.data.adj_t.to(self.device)
+
+    def _prepare_model(self):
+        assert self.args.model_type in ["GCN"]
+        gnn_class = get_model_class(self.args.model_type, self.args.task_type)
+        model = gnn_class(self.args)
+        predictor = LinkPredictor(
+            self.args.num_feats, self.args.gnn_dim_hidden, self.args.gnn_num_layers, self.args.gnn_dropout
+        )
+        return model, predictor
+
+    def _prepare_optimizer(self):
+        return torch.optim.Adam(
+            list(self.model.parameters()) + list(self.predictor.parameters()),
+            lr=self.args.gnn_lr,
+            weight_decay=self.args.gnn_weight_decay,
+        )
+
+    @torch.no_grad()
+    def _get_embeddings(self):
+        return self.model(self.data.x, self.data.edge_index).cpu()
+
+    def training_step(self, epoch):
+        self.model.train()
+        self.predictor.train()
+
+        src_edge = self.split_edge["train"]["source_node"]
+        dst_edge = self.split_edge["train"]["target_node"]
+
+        total_loss, total_example = 0, 0
+        for perm in DataLoader(range(src_edge.size(0)), self.args.gnn_batch_size, shuffle=True):
+            self.optimizer.zero_grad()
+            h = self.model(self.data.x, self.data.adj_t)
+
+            src, dst = src_edge[perm].to(self.device), dst_edge[perm].to(self.device)
+            pos_out = self.predictor(h[src], h[dst])
+            pos_loss = -torch.log(pos_out + 1e-15).mean()
+
+            dst_neg = torch.randint(0, self.data.x.size(0), src.size(), dtype=torch.long).to(self.device)
+            neg_out = self.predictor(h[src], h[dst_neg])
+            neg_loss = -torch.log(1 - neg_out + 1e-15).mean()
+
+            loss = pos_loss + neg_loss
+            loss.backward()
+            self.optimizer.step()
+
+            num_examples = pos_out.size(0)
+            total_loss += loss.item() * num_examples
+            total_examples += num_examples
+
+        return total_loss / total_examples
+
+
+#########################################
+# SEALTrainer
+#########################################
+
+
+class SEALDataset(InMemoryDataset):
+    def __init__(self, dataset, num_hops, split="train"):
+        self.data = dataset[0]
+        self.num_hops = num_hops
+        super().__init__(dataset.root)
+        index = ["train", "val", "test"].index(split)
+        self.data, self.slices = torch.load(self.processed_paths[index])
+
+    @property
+    def processed_file_names(self):
+        return ["SEAL_train_data.pt", "SEAL_val_data.pt", "SEAL_test_data.pt"]
+
+    def process(self):
+        transform = RandomLinkSplit(num_val=0.05, num_test=0.1, is_undirected=True, split_labels=True)
+        train_data, val_data, test_data = transform(self.data)
+
+        self._max_z = 0
+
+        # Collect a list of subgraphs for training, validation and testing:
+        train_pos_data_list = self.extract_enclosing_subgraphs(
+            train_data.edge_index, train_data.pos_edge_label_index, 1
+        )
+        train_neg_data_list = self.extract_enclosing_subgraphs(
+            train_data.edge_index, train_data.neg_edge_label_index, 0
+        )
+
+        val_pos_data_list = self.extract_enclosing_subgraphs(val_data.edge_index, val_data.pos_edge_label_index, 1)
+        val_neg_data_list = self.extract_enclosing_subgraphs(val_data.edge_index, val_data.neg_edge_label_index, 0)
+
+        test_pos_data_list = self.extract_enclosing_subgraphs(test_data.edge_index, test_data.pos_edge_label_index, 1)
+        test_neg_data_list = self.extract_enclosing_subgraphs(test_data.edge_index, test_data.neg_edge_label_index, 0)
+
+        # Convert node labeling to one-hot features.
+        for data in chain(
+            train_pos_data_list,
+            train_neg_data_list,
+            val_pos_data_list,
+            val_neg_data_list,
+            test_pos_data_list,
+            test_neg_data_list,
+        ):
+            # We solely learn links from structure, dropping any node features:
+            data.x = F.one_hot(data.z, self._max_z + 1).to(torch.float)
+
+        torch.save(self.collate(train_pos_data_list + train_neg_data_list), self.processed_paths[0])
+        torch.save(self.collate(val_pos_data_list + val_neg_data_list), self.processed_paths[1])
+        torch.save(self.collate(test_pos_data_list + test_neg_data_list), self.processed_paths[2])
+
+    def extract_enclosing_subgraphs(self, edge_index, edge_label_index, y):
+        data_list = []
+        for src, dst in edge_label_index.t().tolist():
+            sub_nodes, sub_edge_index, mapping, _ = k_hop_subgraph(
+                [src, dst], self.num_hops, edge_index, relabel_nodes=True
+            )
+            src, dst = mapping.tolist()
+
+            # Remove target link from the subgraph.
+            mask1 = (sub_edge_index[0] != src) | (sub_edge_index[1] != dst)
+            mask2 = (sub_edge_index[0] != dst) | (sub_edge_index[1] != src)
+            sub_edge_index = sub_edge_index[:, mask1 & mask2]
+
+            # Calculate node labeling.
+            z = self.drnl_node_labeling(sub_edge_index, src, dst, num_nodes=sub_nodes.size(0))
+
+            data = Data(x=self.data.x[sub_nodes], z=z, edge_index=sub_edge_index, y=y)
+            data_list.append(data)
+
+        return data_list
+
+    def drnl_node_labeling(self, edge_index, src, dst, num_nodes=None):
+        # Double-radius node labeling (DRNL).
+        src, dst = (dst, src) if src > dst else (src, dst)
+        adj = to_scipy_sparse_matrix(edge_index, num_nodes=num_nodes).tocsr()
+
+        idx = list(range(src)) + list(range(src + 1, adj.shape[0]))
+        adj_wo_src = adj[idx, :][:, idx]
+
+        idx = list(range(dst)) + list(range(dst + 1, adj.shape[0]))
+        adj_wo_dst = adj[idx, :][:, idx]
+
+        dist2src = shortest_path(adj_wo_dst, directed=False, unweighted=True, indices=src)
+        dist2src = np.insert(dist2src, dst, 0, axis=0)
+        dist2src = torch.from_numpy(dist2src)
+
+        dist2dst = shortest_path(adj_wo_src, directed=False, unweighted=True, indices=dst - 1)
+        dist2dst = np.insert(dist2dst, src, 0, axis=0)
+        dist2dst = torch.from_numpy(dist2dst)
+
+        dist = dist2src + dist2dst
+        dist_over_2, dist_mod_2 = dist // 2, dist % 2
+
+        z = 1 + torch.min(dist2src, dist2dst)
+        z += dist_over_2 * (dist_over_2 + dist_mod_2 - 1)
+        z[src] = 1.0
+        z[dst] = 1.0
+        z[torch.isnan(z)] = 0.0
+
+        self._max_z = max(int(z.max()), self._max_z)
+
+        return z.to(torch.long)
+
+
+class SEALTrainer(LinkTrainer):
+    pass
